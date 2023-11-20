f8ecd44bd004a962678d0b958374922061387deb
Vanshikanagar <92163660+Vanshikanagar@users.noreply.github.com>
2023-11-20 16:39:29 +0530

    Update marketplace_services.sql

--- marketplace_services.sql
+++ marketplace_services.sql
@@ -3,10 +3,9 @@
 ** Mini Project                       **
 **                                    **
 ** Authors:                           **
-**   - Rupak Biswas                   **
-**   - Aman Agarwal                   **
+**   - Vanshika Nagar                **
 **   - Sanskriti Singh                **
-**   - Dinesh Chukkala                **
+**                                     **
 **                                    **
 ** File: marketplace_services.sql     **
 ****************************************
